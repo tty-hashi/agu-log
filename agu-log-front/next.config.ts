14c862c9faa3e6e1d3bfeda64e42008d537e3622
tty-hashi <hashi.ndig@gmail.com>
2025-03-10 09:19:33 +0900

    set: locatorjs が使用できないので code-inspector を導入して turbopack を不使用とした

--- agu-log-front/next.config.ts
+++ agu-log-front/next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from 'next'
+const { codeInspectorPlugin } = require('code-inspector-plugin')
 
 const nextConfig: NextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'], // Google の画像ドメインを許可
+  },
+  webpack: (config, { dev, isServer }) => {
+    config.plugins.push(codeInspectorPlugin({ bundler: 'webpack' }))
+    return config
   },
 }
 
