11649a8636b9935b4c960d9aa8fa6f579f6e5a93
tty-hashi <hashi.ndig@gmail.com>
2025-03-18 09:57:19 +0900

    feat: 記事タイプの追加

--- agu-log-front/src/features/posts/post/PostView.tsx
+++ agu-log-front/src/features/posts/post/PostView.tsx
@@ -6,6 +6,7 @@ import { LikeButton } from './components/LikeButton/LikeButton'
 import { PostWithRelations } from '@/app/[username]/articles/[postId]/page'
 import CommentSection from './components/Comments/CommentSection'
 import { EditButton } from './components/EditButton/EditButton'
+import { Badge } from '@/components/ui/badge'
 
 type ArticleViewProps = {
   post: PostWithRelations
@@ -44,6 +45,15 @@ export default function PostView({ post, currentUserId }: ArticleViewProps) {
             <div>
               <div className='font-semibold'>{post.author.profile?.displayName || 'ゲスト'}</div>
               <div className='text-sm text-muted-foreground'>{timeAgo}</div>
+            </div>
+            <div className=''>
+              <Badge variant='outline' className='shrink-0'>
+                {post.type === 'diary' && '日記'}
+                {post.type === 'poem' && 'ポエム'}
+                {post.type === 'tech' && '技術'}
+                {post.type === 'question' && '質問'}
+                {post.type === 'review' && 'レビュー'}
+              </Badge>
             </div>
           </div>
         </div>
--- agu-log-front/src/validations/schemas/post.ts
+++ agu-log-front/src/validations/schemas/post.ts
@@ -4,6 +4,10 @@ import { z } from 'zod'
 export const PostStatus = z.enum(['draft', 'published'])
 export type PostStatus = z.infer<typeof PostStatus>
 
+// 記事タイプを定義
+export const PostType = z.enum(['diary', 'poem', 'tech', 'question', 'review'])
+export type PostType = z.infer<typeof PostType>
+
 // 記事更新時のバリデーションスキーマ
 export const updatePostSchema = z.object({
   title: z
@@ -15,6 +19,7 @@ export const updatePostSchema = z.object({
     .min(1, '記事内容は必須です')
     .max(50000, '記事内容は50000文字以内で入力してください'),
   status: PostStatus,
+  type: PostType,
   tagIds: z.array(z.string()).optional(),
   postId: z.string(),
 })
